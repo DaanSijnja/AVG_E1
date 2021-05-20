9d8e3a9ce72d3f8ea91ef9e4cbb50435059451df
DaanSijnja <68638963+DaanSijnja@users.noreply.github.com>
2021-05-20 17:56:04 +0200

    Added libs
    -servo.h
    -servo.c

--- Project_AVG_E1/main.c
+++ Project_AVG_E1/main.c
@@ -2,13 +2,18 @@
  */
 
 #include <avr/io.h>
+#include "lib/servo.h"
 
 int main(void)
 {
 
     // Insert code
-
+    init_servo();
     while(1)
+    {
+        servo1_set_percentage(100);
+        servo2_set_percentage(100);
+    }
     ;
 
     return 0;
